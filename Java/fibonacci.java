2f36e3cd175701d0ba22e209c31335f8a7868f7d
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2019-07-09 15:51:31 +0200

    Create fibonacci.java
    Dynamic programming

---
+++ Java/fibonacci.java
@@ -0,0 +1,24 @@
+public class test{
+	public static void main(String args[]){
+		System.out.println(fib(6));
+	}
+	public static int fib(int n){
+		if(n==0 || n==1){
+			return n;
+		}else{
+			int [] fibs=new int [n+1];
+			fibs[1]=1;
+			return helper(fibs,n);
+		}
+	}
+	public static int helper(int [] fibs,int n){
+		if(fibs[n]!=0){
+			return fibs[n];
+		}else if(n==0){
+			return 0;
+		}else{
+			fibs[n]=helper(fibs,n-1)+helper(fibs,n-2);
+			return fibs[n];
+		}
+	}
+}
