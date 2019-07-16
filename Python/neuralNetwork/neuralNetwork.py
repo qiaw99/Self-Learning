b44b172fa5fb75d6629ca8b68ff5a7d7c1ec7cf8
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2019-07-16 19:02:38 +0200

    Create neuralNetwork.py

---
+++ Python/neuralNetwork/neuralNetwork.py
@@ -0,0 +1,17 @@
+#neural network class definition
+class neuralNetwork:
+    #initialise the neural network
+    def __init__(self,inputnodes,hiddennodes,outputnodes,lerningrate):
+        #set number of nodes in each input, hidden, output layer
+        self.inodes=inputnodes
+        self.hnodes=hiddennodes
+        self.onodes=outputnodes
+        #lerning rate
+        self.lr=lerningrate
+        
+    #train the neural network
+    def train():
+        pass
+    #query the neural network
+    def query():
+        pass
