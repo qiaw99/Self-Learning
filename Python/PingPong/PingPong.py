55e1e6da630567c74da75b6f6cbbec2f97dba339
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2019-07-31 12:50:18 +0800

    Update PingPong.py

--- Python/PingPong/PingPong.py
+++ Python/PingPong/PingPong.py
@@ -1,5 +1,17 @@
-import os
 import turtle
+
+#Score
+score_a=0
+score_b=0
+
+#Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0,260)
+pen.write("Player A: 0  Player B: 0", align = "center", font = ("Courier",24,"normal"))
 
 wn = turtle.Screen()
 wn.title("Pong by @qiaw99")
@@ -93,9 +105,25 @@ while(True):
 	if(ball.xcor()<-390):
 		ball.goto(0,0)
 		ball.dx *= -1
+		score_b += 1
+		pen.clear()
+		pen.write("Player A: {}  Player B: {}".format(score_a,score_b), align = "center", font = ("Courier",24,"normal"))
 
 	if(ball.xcor()>390):
 		ball.goto(0,0)
 		ball.dx *= -1
+		score_a += 1
+		pen.clear()
+		pen.write("Player A: {}  Player B: {}".format(score_a,score_b), align = "center", font = ("Courier",24,"normal"))
+
+
+	#Paddle and collision
+	if(ball.xcor()>340 and ball.xcor()<350 and ball.ycor()<paddle_b.ycor()+40 and ball.ycor()> paddle_b.ycor()-40):
+		ball.setx(340)
+		ball.dx *= -1
+
+	if(ball.xcor()<-340 and ball.xcor()>-350 and ball.ycor()<paddle_a.ycor()+40 and ball.ycor()> paddle_a.ycor()-40):
+		ball.setx(-340)
+		ball.dx *= -1	
 
 wn.mainloop()	
