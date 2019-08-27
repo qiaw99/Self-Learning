97427243fd9db27b7bfc3c62cc57444f56da854c
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2019-08-27 19:26:45 +0800

    Update cut_rod.py

--- Python/Data_Structure/Dynamic Programming/cut_rod.py
+++ Python/Data_Structure/Dynamic Programming/cut_rod.py
@@ -36,10 +36,31 @@ def bottom_up_cut_rod(p,n):
         r[j] = q
     return r[n]
 
+def extended_bottom_up_cut_rod(p,n):
+    r = [-1 for i in range(0,n+1)]
+    s = [-1 for i in range(0,n+1)]
+    r[0] = 0
+    for j in range(1,n+1):
+        q = -1  
+        for i in range(1,j+1):
+            if(q < p[i] + r[j-i]):
+                q = p[i] +r[j-i]
+                s[j] = i
+                r[j] = q
+    return (r,s)
+
+def print_cut_rod_solution(p,n):
+    (r,s) = extended_bottom_up_cut_rod(p,n)
+    while(n > 0):
+        print(s[n],end=' ')
+        n = n-s[n]
+
+
 def main():
     print(cut_rod([1,5,8,9,10,17,17,20,24,30],5))
     print(memorized_cut_rod([1,5,8,9,10,17,17,20,24,30],5))
     print(bottom_up_cut_rod([1,5,8,9,10,17,17,20,24,30],5))
+    print_cut_rod_solution([1,5,8,9,10,17,17,20,24,30],5)
 
 if(__name__ == "__main__"):
     main()
