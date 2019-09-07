3d5e82518571563b92083feeab4596654200d640
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2019-09-07 19:50:03 +0800

    Create Bounded_Resource.java

---
+++ Java/MultipleThreads_and_Sockets/Bounded_Resource/Bounded_Resource.java
@@ -0,0 +1,68 @@
+package test;
+
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
+class Log{
+	public static void println(String s) {
+		System.out.println(Thread.currentThread().getName() + ": " + s);
+	}
+}
+
+//资源个数有限
+class BoundedResource{
+	private final Semaphore semaphore;
+	private final int permits;
+	private final static Random random = new Random(314159);
+	
+	public BoundedResource(int permits) {
+		this.semaphore = new Semaphore(permits);
+		this.permits = permits;
+	}
+	
+	//使用资源
+	public void use() throws InterruptedException{
+		semaphore.acquire();
+		try {
+			doUse();
+		}finally {
+			semaphore.release();
+		}
+	}
+	
+	//实际使用资源，此处仅使用Thread.sleep
+	protected void doUse() throws InterruptedException{
+		Log.println("BEGIN: use = " + (permits - semaphore.availablePermits()));
+		Thread.sleep(random.nextInt(500));
+		Log.println("END: use = " + (permits - semaphore.availablePermits()));
+	}
+}
+
+class UserThread extends Thread{
+	private final static Random random = new Random(26535);
+	private final BoundedResource resource;
+	
+	public UserThread(BoundedResource resource) {
+		this.resource = resource;
+	}
+	
+	public void run() {
+		try {
+			while(true) {
+				resource.use();
+				Thread.sleep(random.nextInt());
+			}
+		}catch(InterruptedException e){
+			e.printStackTrace();
+		}
+	}
+}
+
+public class Bounded_Resource{
+	public static void main(String args[]) {
+		BoundedResource resource = new BoundedResource(3);
+		for(int i = 0; i<10; i++) {
+			new UserThread(resource).start();
+		}
+	}
+}
