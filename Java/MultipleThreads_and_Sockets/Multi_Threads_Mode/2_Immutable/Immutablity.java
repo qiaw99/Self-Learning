05d6095eec0282fd8b986776db9cb0463c3328b0
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2019-09-07 21:00:15 +0800

    Update Immutablity.java

--- Java/MultipleThreads_and_Sockets/Multi_Threads_Mode/2_Immutable/Immutablity.java
+++ Java/MultipleThreads_and_Sockets/Multi_Threads_Mode/2_Immutable/Immutablity.java
@@ -1,11 +1,5 @@
 package test;
 
-/*
- * "Immutable Mode"
- * Java.lang.String表示字符串，但是在类中并没有修改字符串的方法，
- * 也就说，String的实例所表示的内容不会发生变化
- */
-
 final class Person{
 	private final String name;
 	private final String address;
