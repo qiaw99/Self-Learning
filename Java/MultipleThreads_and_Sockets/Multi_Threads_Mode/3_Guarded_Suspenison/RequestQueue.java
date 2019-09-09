f5b104ca45e9850da22d8e2221e6444caa5eac1e
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2019-09-09 18:23:37 +0800

    2. version of RequestQueue

--- Java/MultipleThreads_and_Sockets/Multi_Threads_Mode/3_Guarded_Suspenison/RequestQueue.java
+++ Java/MultipleThreads_and_Sockets/Multi_Threads_Mode/3_Guarded_Suspenison/RequestQueue.java
@@ -4,6 +4,11 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class RequestQueue{
    private final BlockingQueue<Request> queue = new LinkedBlockingQueue<Request>();
    
+    /**
+    * take(): take out the first element in the queue
+    * put(): put a new element at the tail of the queue
+    * @return
+    */
    public Request getRequest(){
 	   Request req = null;
 	   try {
