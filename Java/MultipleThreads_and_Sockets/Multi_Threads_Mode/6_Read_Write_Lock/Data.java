fbaf5fe27123a028de15076148606a5eeb76342f
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2019-09-12 15:53:33 +0800

    2. version of class Data

---
+++ Java/MultipleThreads_and_Sockets/Multi_Threads_Mode/6_Read_Write_Lock/Data.java
@@ -0,0 +1,61 @@
+package test;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class Data{
+	private final char[] buffer;
+	private final ReadWriteLock lock = new ReentrantReadWriteLock(true /*false*/ );
+	private final Lock readLock = lock.readLock();
+	private final Lock writeLock = lock.writeLock();
+	
+	public Data(int size) {
+		this.buffer = new char[size];
+		for(int i = 0; i < buffer.length; i++) {
+			buffer[i] = '*';
+		}
+	}
+	
+	public char[] read() throws InterruptedException{
+		readLock.lock();
+		try {
+			return doRead();
+		}finally {
+			readLock.unlock();
+		}
+	}
+	
+	public void write(char c) throws InterruptedException{
+		writeLock.lock();
+		try {
+			doWrite(c);
+		}finally {
+			writeLock.unlock();
+		}
+	}
+	
+	private char[] doRead() {
+		char[] newbuff = new char[buffer.length];
+		for(int i = 0; i < buffer.length; i++) {
+			newbuff[i] = buffer[i];
+		}
+		slowly();
+		return newbuff;
+	}
+	
+	private void doWrite(char c) {
+		for(int i = 0; i < buffer.length; i++) {
+			buffer[i] = c;
+			slowly();
+		}
+	}
+	
+	private void slowly() {
+		try {
+			Thread.sleep(50);
+		}catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
