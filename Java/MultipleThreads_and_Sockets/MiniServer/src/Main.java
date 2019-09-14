c2f81d7d6ecf8ed293edfceb9dc7303aa8736e5c
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2019-09-14 13:19:26 +0800

    Create Main.java

---
+++ Java/MultipleThreads_and_Sockets/MiniServer/src/Main.java
@@ -0,0 +1,13 @@
+package server;
+
+import java.io.IOException;
+
+public class Main {
+	public static void main(String args[]) {
+		try {
+			new MiniServer(8888).execute();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
