deae11fd7a0606f5b34ef199c4115d3fa30eae40
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2019-11-01 09:13:58 +0100

    Create index.js

---
+++ JavaScript/Index/index.js
@@ -0,0 +1,35 @@
+console.log("Hello World");
+
+// String Literal
+let name = "Test";
+console.log(name);
+
+// Numer Literal
+let age = 30;
+
+// Boolean Literal
+let isApproved = false;
+
+let firstName = undefined;
+
+const interstRate = 0.3;
+//interstRate = 1;
+console.log(interstRate);
+
+let person = {
+    name: "Mosh",
+    age: 30
+};
+
+// Dot Notation
+person.name ['John'];
+
+// Bracket Notation
+person['name'] = 'Mary';
+
+console.log(person);
+
+// Dynamic
+let selectedColor = ['red','blue'];
+selectedColor[2] = 'green'
+console.log(selectedColor)
