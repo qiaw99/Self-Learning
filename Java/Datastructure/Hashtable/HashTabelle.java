70bf009ae78ff52c3b2c5d27aee87ac52cb5b672
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2020-01-03 11:31:43 +0100

    Update HashTabelle.java

--- Java/Datastructure/Hashtable/HashTabelle.java
+++ Java/Datastructure/Hashtable/HashTabelle.java
@@ -5,28 +5,80 @@ class HashTable<Integer, V>{
 	int size;
 	int counter;
 	Slot<Integer, V>[] slots;
+	boolean deleted[];
 	
 	@SuppressWarnings("unchecked")
 	public HashTable(int size) {
 		this.size = size;
 		this.counter = 0;
 		this.slots = new Slot[size];
+		this.deleted = new boolean[this.size];
+		initialize();
+	}
+	
+	private int hash(Object key) {
+		return (int)key % size;
 	}
 	
 	public int getNumberOfElement() {
 		return this.counter;
 	}
 	
+	private boolean isEmpty() {
+		return size == 0;
+	}
+	
+	private boolean isFull() {
+		if(counter < size) {
+			return false;
+		}else {
+			return true;
+		}
+	}
+	
+	private void initialize() {
+		for(int i = 0; i < this.size; i++) {
+			this.deleted[i] = true;
+		}
+	}
+	
+	public void printDeleted() {
+		for(int i = 0; i < this.size; i++) {
+			System.out.println(this.deleted[i]);
+		}
+	}
+	
 	public void printHashTable() {
 		for(int i = 0; i < size; i++) {
 			if(slots[i] != null) {
-				System.out.println(slots[i].value);
+				System.out.println("The value = " + slots[i].value + ", the index = " + i + ", deleted = " + deleted[i]);
 			}
 		}
 	}
 	
-	public int hash(Object key) {
-		return (int)key % (size - 1);
+	public void delete(int key) throws Exception{
+		if(isEmpty()) {
+			throw new Exception("The hashtable is empty!");
+		}else if(key > this.size - 1 || key < 0) {
+			throw new IndexOutOfBoundsException();
+		}else {
+			deleted[key] = true;
+		}
+	}
+	
+	public int search(int key) {
+		int j;
+		int i = 0;
+
+		while(i != this.size || slots[i] != null) {
+			j = hash(key);
+			if(slots[j].key.equals(key) && deleted[j] == false) {
+				return j;
+			}
+		}
+		
+		// -1 means error
+		return -1;
 	}
 	
 	public void insert(Slot<Integer, V> slot, Slot<Integer, V>[] slots) throws Exception {
@@ -35,21 +87,15 @@ class HashTable<Integer, V>{
 		}else {
 			int index = hash(slot.key);
 			counter ++;
-			
-			while(slots[index] != null) {
-				++ index;
-				index %= size;
+			while(deleted[index] != true) {
+				if(++ index > size - 1) {
+					index = 0;
+				}
 			}
-			slots[index - 1] = slot;
+			
+			slots[index] = slot;
+			deleted[index] = false;
 		}	
-	}
-	
-	public boolean isFull() {
-		if(counter < size) {
-			return false;
-		}else {
-			return true;
-		}
 	}
 	
 	public boolean contain(Integer key) {
@@ -77,12 +123,20 @@ class Slot<Integer, V>{
 }
 
 public class HashTabelle {
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		HashTable<Integer, String> hashTable = new HashTable<Integer, String>(5);
-		System.out.println(hashTable.size);
-		//System.out.println(hashTable.contain((Integer)5));
+		
 		try {
-			hashTable.insert(new Slot<Integer, String>(2, "sth"), hashTable.slots);
+			hashTable.insert(new Slot<Integer, String>(3, "sth"), hashTable.slots);
+			hashTable.insert(new Slot<Integer, String>(1, "sth1"), hashTable.slots);
+			hashTable.insert(new Slot<Integer, String>(2, "sth2"), hashTable.slots);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+		try {
+			hashTable.delete(2);
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
