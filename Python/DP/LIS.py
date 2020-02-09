b5486637c5d39daabc0a3df4b85b1df967983508
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2020-02-09 15:40:35 +0100

    Update LIS.py

--- Python/DP/LIS.py
+++ Python/DP/LIS.py
@@ -5,6 +5,5 @@ maxResult = 0
 for i in range(1, len(nums)):
     for j in range(0, i):
         if(nums[j] < nums[i]):
-            dp[i] = max(dp[i], dp[j] + 1)
-    maxResult = max(maxResult, dp[i])
-print(maxResult)    
+            dp[i] = max(dp[i], dp[j] + 1)   
+print(dp[-1])
