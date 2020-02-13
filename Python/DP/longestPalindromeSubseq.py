d601fe7d0c979534ac612a0a11aace8d8ac1eb94
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2020-02-13 20:05:48 +0100

    Create longestPalindromeSubseq.py

---
+++ Python/DP/longestPalindromeSubseq.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        dp = [[0 for _ in range(len(s) + 1)] for _ in range(len(s) + 1)]
+        for i in range(len(s) + 1):
+            for j in range(len(s) + 1):
+                if(i == j):
+                    dp[i][j] = 1
+        for i in range(len(s) - 1, -1, -1):
+            for j in range(i + 1, len(s)):
+                if(s[i] == s[j]):
+                    dp[i][j] = dp[i + 1][j - 1] + 2
+                else:
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
+        return dp[0][len(s) - 1]
